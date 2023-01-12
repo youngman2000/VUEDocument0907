bc728bc17cb6582f7f70a3f2de1933b38504b6ad
youngsuper <1406422800@qq.com>
2023-01-13 01:51:28 +0800

    update

--- src/views/quickStart.vue
+++ src/views/quickStart.vue
@@ -2,7 +2,7 @@
  * @Author: youngsuper 1406422800@qq.com
  * @Date: 2022-09-07 23:32:15
  * @LastEditors: youngsuper 1406422800@qq.com
- * @LastEditTime: 2023-01-13 01:44:47
+ * @LastEditTime: 2023-01-13 01:45:08
  * @FilePath: /VUEDocument0907/src/views/quickStart.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
