e3322af9480515ff069c5853cf72aba539f7106b
youngsuper <1406422800@qq.com>
2023-01-13 01:45:08 +0800

    update

--- src/views/code/quickStart.js
+++ src/views/code/quickStart.js
@@ -2,7 +2,7 @@
  * @Author: youngsuper 1406422800@qq.com
  * @Date: 2023-01-12 23:57:44
  * @LastEditors: youngsuper 1406422800@qq.com
- * @LastEditTime: 2023-01-13 01:17:00
+ * @LastEditTime: 2023-01-13 01:42:26
  * @FilePath: /VUEDocument0907/src/views/code/quickStart.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -68,6 +68,6 @@ Done.`,
         return {message:'Hello Vue!'}
       }
     }).mount("#app")
-  `
+  `,
 };
-export { code };
+export { code }
